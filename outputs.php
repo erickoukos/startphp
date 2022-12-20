3232533fd1c841ff9b2ed628f774e1c9d140d7ad
erickoukos <erickoukos@gmail.com>
2022-12-20 21:14:14 +0300

    Intro to various Outputs and Variables

---
+++ outputs.php
@@ -0,0 +1,45 @@
+<?php
+/* -------- Output & Comments ------- */
+
+/*
+  A .php file running on a server, can output both HTML and PHP code. 
+  There are multiple functions that can be used to output data to the browser.
+*/
+// single line comment
+// echo - output strings, numbers, html, etc
+
+
+
+// print - Similar to echo, but slower
+
+
+// print_r - gives more info - usually for arrays
+
+
+// var_dump - even more info compared to print_r
+
+// escaping chars using backslash
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PhP inside Html</title>
+</head>
+<body>
+  <p>write your php here</p>
+
+  <br>
+
+  <?php 
+
+// some php here too
+
+   ?>
+</body>
+</html>
