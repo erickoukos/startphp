eb719801ed3542e56132d52aca366bc27751f2a3
erickoukos <erickoukos@gmail.com>
2023-01-03 20:48:01 +0300

    Conditionals Done! - ends

---
+++ loops.php
@@ -0,0 +1,64 @@
+<?php
+
+/* -------- Loops & Iteration ------- */
+
+/* ------------ For Loop ------------ */
+
+/*
+** For Loop Syntax
+  for (initialize; condition; increment) {
+  // code to be executed
+  }
+*/
+
+
+
+/* ------------ While Loop ------------ */
+
+/*
+** While Loop Syntax
+  while (condition) {
+  // code to be executed
+  }
+*/
+
+
+
+
+/* ---------- Do While Loop --------- */
+
+/*
+** Do While Loop Syntax
+  do {
+  // code to be executed
+  } while (condition);
+
+do...while loop will always execute the block of code once, even if the condition is false.
+*/
+
+
+
+/* ---------- Foreach Loop ---------- */
+
+/*
+** Foreach Loop Syntax
+  foreach ($array as $value) {
+  // code to be executed
+  }
+*/
+
+// Loop through an array
+
+
+
+
+// Use the indexes within the loop
+
+
+
+
+// Use the keys within the loop for an associative array
+
+
+
+// Get the Keys
