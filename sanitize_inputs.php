e0405e530b7e62839b2071af62bd8f2b55fbee6d
erickoukos <erickoukos@gmail.com>
2023-01-16 20:31:12 +0300

    Sanitizing Inputs Session Completed and Updated

--- sanitize_inputs.php
+++ sanitize_inputs.php
@@ -4,24 +4,21 @@
 /*
   Data submitted through a form is not sanitized by default. We have methods to sanitize data manually.
 */
+
 if (isset($_POST['submit'])) {
-  // $username = $_POST['username'];
+  // $name = $_POST['email'];
   // $email = $_POST['email'];
-  // htmlspecialchars - Converts special chars to HTML entities
-  $username = filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-  $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-
- // htmlspecialchars() - Convert special characters to HTML entities
-  // $username = htmlspecialchars($_POST['username']);
+  // htmlspecialchars() - Convert special characters to HTML entities
+  // $name = htmlspecialchars($_POST['name']);
   // $email = htmlspecialchars($_POST['email']);
 
   // filter_var() - Sanitize data
-  $username = filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-  $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+  // $name = filter_var($_POST['name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  // $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
   // filter_input() - Sanitize inputs
-  $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 
   // FILTER_SANITIZE_STRING - Convert string to string with only alphanumeric, whitespace, and the following characters - _.:/
@@ -29,36 +26,24 @@ if (isset($_POST['submit'])) {
   // FILTER_SANITIZE_URL - Convert string to a valid URL
   // FILTER_SANITIZE_NUMBER_INT - Convert string to an integer
   // FILTER_SANITIZE_NUMBER_FLOAT - Convert string to a float
-  // FILTER_SANITIZE_FULL_SPECIAL_CHARS - HTML-encodes special characters, keeps spaces and most other characters  
-}
+  // FILTER_SANITIZE_FULL_SPECIAL_CHARS - HTML-encodes special characters, keeps spaces and most other characters
+} ?>
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SANITIZING DATA INPUTS</title>
-</head>
-<body>
-  <!-- Passing Data through a form -->
-  <h1>Sanitizing Data in Forms</h1>
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
- <div>
- <label for="Username">Username:</label>
-  <input type="text" name="username" id="username">
-  <br><br>
- 
-  <?php echo $email; ?>
-</div>
+<!-- Pass data through a form -->
+<!-- php_self can be used for xss -->
+<form action="<?php echo htmlspecialchars(
+  $_SERVER['PHP_SELF']
+); ?>" method="POST">
 <div>
-    <label for="Email">Email Add:</label>
-    <input type="email" name="email" id="email">
-  </div>
- <br>
- <input type="submit" value="Submit">
+  <label>Name: </label>
+  <input type="text" name="name">
+</div>
+<br>
+<?php echo $email; ?>
+<div>
+<label>Email: </label>
+  <input type="email" name="email">
+</div>
+<br>
+  <input type="submit" name="submit" value="Submit">
 </form>
-</body>
-</html>
