396d8cbfef029196f2f819034de47b08aaf05fca
erickoukos <erickoukos@gmail.com>
2023-01-18 20:06:35 +0300

    Cookies and Sessions, assign

---
+++ sessions.php
@@ -0,0 +1,11 @@
+<?php
+/* ------------SESSIONS--------*/
+/*Alternatives of cookies, usually a file or a record in a db 
+69i57j0i512l4j69i60l3.5475j0j7
+4765445b-32c6-49b0-83e6-1d93765276ca
+
+session_start()
+session_unset()
+session_destroy()
+ * 
+ */
